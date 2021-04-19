bc831bf9bb738f390fc55deb11485208c9c20fa5
vestigialcode <dibyajiban232@gmail.com>
2021-04-19 20:15:49 +0530

    Added URL input

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
    const puppeteer = require('puppeteer');
+   const readline = require('readline-sync');
+
+let name = readline.question("Type/Paste the complete link (ex. http://example.com )\n");
+
+console.log("Input URL is " + name );
 
     (async function main() {
       try {
         const browser = await puppeteer.launch();
         const [page] = await browser.pages();
+        await page.setDefaultNavigationTimeout(0);
 
-        await page.goto('https://example.org/', { waitUntil: 'networkidle0' });
+        await page.goto(name, {   waitUntil: 'load',
+        // Remove the timeout
+        timeout: 0 });
         const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 
         console.log(data);
